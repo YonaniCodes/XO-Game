8ca5a5328034472ca27cce2597da330351bbd351
Yonanicodes <yonasawokeyitay@gmail.com>
2024-01-12 22:44:14 +0300

    version 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Moves = [, , , , , , , , ,];
+console.log(Moves.length);
 let isItTurnOfX = true;
-
+let winner;
 startTheGame();
 function startTheGame() {
   gameBoardHTML = `
@@ -23,8 +24,23 @@ function startTheGame() {
   });
 
   restoreMoves();
+  winner = undefined;
+  showTheCuurentMove();
 }
+function showTheCuurentMove() {
+  if (!winner && isItTurnOfX) {
+    document.querySelector(".X").style.backgroundColor = "var(--primary-color)";
+    document.querySelector(".O").style.backgroundColor =
+      "var(--tertiary-color) ";
+    document.querySelector(".O").style.color = "var(--secondary-color)";
+  } else {
+    console.log("nowinnner");
+    document.querySelector(".O").style.backgroundColor = "rgb(61, 168, 168)";
+    document.querySelector(".O").style.color = "var(--tertiary-color)";
 
+    document.querySelector(".X").style.backgroundColor = "white";
+  }
+}
 function makeMove(box) {
   box.innerHTML =
     box.innerHTML === ""
@@ -36,12 +52,16 @@ function makeMove(box) {
   storeMoves(box);
   checkForWin();
   toggleMoveTurn();
+  showTheCuurentMove();
 }
 
 function styleMoves(box) {
-  console.log("styling");
-  if (isItTurnOfX) box.style.color = "rgb(48, 53, 53);";
-  else box.style.color = "white";
+  if (isItTurnOfX) {
+    box.style.color = "var(--secondary-color)";
+
+    return;
+  }
+  box.style.color = "var(--quaternary-color)";
 }
 
 function storeMoves(box) {
@@ -56,56 +76,102 @@ function toggleMoveTurn() {
 
 function checkForWin() {
   if (Moves[0] === Moves[1] && Moves[1] === Moves[2] && Moves[0] != undefined) {
-    restartTheGame(0, 1, 2);
+    celebrateWin(0, 1, 2);
+
+    winner = isItTurnOfX ? "X" : "O";
+    setTimeout(() => renderMatchWinner(true), 500);
 
     return true;
   }
   if (Moves[3] === Moves[4] && Moves[4] === Moves[5] && Moves[3] != undefined) {
-    restartTheGame(3, 4, 5);
+    celebrateWin(3, 4, 5);
+    winner = isItTurnOfX ? "X" : "O";
+    setTimeout(() => renderMatchWinner(true), 500);
 
     return true;
   }
   if (Moves[6] === Moves[7] && Moves[7] === Moves[8] && Moves[6] != undefined) {
-    restartTheGame(6, 7, 8);
+    celebrateWin(6, 7, 8);
+    winner = isItTurnOfX ? "X" : "O";
+    setTimeout(() => renderMatchWinner(true), 500);
+
     return true;
   }
   if (Moves[0] === Moves[3] && Moves[3] === Moves[6] && Moves[0] != undefined) {
-    restartTheGame(0, 3, 6);
-
-    setTimeout(restartTheGame, 1000);
+    celebrateWin(0, 3, 6);
+    removeEventHandler(".box", makeMove);
+    winner = isItTurnOfX ? "X" : "O";
+    setTimeout(() => renderMatchWinner(true), 500);
 
     return true;
   }
   if (Moves[1] === Moves[4] && Moves[4] === Moves[7] && Moves[1] != undefined) {
-    restartTheGame(1, 4, 7);
-    console.log("line 5 checking");
+    celebrateWin(1, 4, 7);
+    winner = isItTurnOfX ? "X" : "O";
 
+    setTimeout(() => renderMatchWinner(true), 500);
     return true;
   }
   if (Moves[2] === Moves[5] && Moves[5] === Moves[8] && Moves[2] != undefined) {
-    restartTheGame(2, 5, 8);
+    celebrateWin(2, 5, 8);
+    winner = isItTurnOfX ? "X" : "O";
 
+    setTimeout(() => renderMatchWinner(true), 500);
     return true;
   }
   if (Moves[2] === Moves[4] && Moves[4] === Moves[6] && Moves[2] != undefined) {
-    restartTheGame(2, 4, 6);
+    celebrateWin(2, 4, 6);
+    winner = isItTurnOfX ? "X" : "O";
+    setTimeout(() => renderMatchWinner(true), 500);
 
     return true;
   }
   if (Moves[0] === Moves[4] && Moves[4] === Moves[8] && Moves[0] != undefined) {
-    restartTheGame(0, 4, 8);
+    celebrateWin(0, 4, 8);
+    winner = isItTurnOfX ? "X" : "O";
 
+    setTimeout(() => renderMatchWinner(true), 500);
     return true;
   }
   if (!Moves.includes(undefined)) {
-    setTimeout(startTheGame, 1000);
+    setTimeout(() => renderMatchWinner(false), 500);
   }
 }
+/* 
+function removeEventHandler() {
+  let boxes = document.querySelectorAll(".box");
 
-function restartTheGame(box1, box2, box3) {
+  boxes.forEach((box) => {
+    box.removeEventlistner(makeMove(box));
+    console.log("removed");
+  });
+}
+*/
+
+function renderMatchWinner(hasWinner) {
+  let ele = document.createElement("div");
+  ele.className = "popup";
+  let html = ` <div class="popup">
+  <div class="win-notification">
+  <div class="player">
+ ${
+   hasWinner
+     ? `<span class="${winner}">${winner}</span>`
+     : `<span class="X">X</span> <span class="O">O</span>`
+ }
+  
+  </div>
+  <h1>${hasWinner ? "WINS" : "DRAW"}!</h1>
+</div>
+<div   class="restart">Restart Game</div></div>   `;
+
+  document.querySelector(".game-board").innerHTML = html;
+  document.querySelector(".restart").addEventListener("click", startTheGame);
+}
+function celebrateWin(box1, box2, box3) {
   celebrateWin(box1, box2, box3);
 
-  setTimeout(startTheGame, 1000);
+  setTimeout(startTheGame, 500);
 }
 
 function celebrateWin(box1, box2, box3) {
